70c62eb483009333946cc3fd1887f8dab1a09f60
Kajetan Krasoń <kajetan@czaplinek.pl>
2022-04-02 22:01:57 +0200

    Initial commit

---
+++ CPSiO-Lab-Program.py
@@ -0,0 +1,10 @@
+print("""
+--------------------------------------------------------------------
+|Laboratorium z przedmiotu Cyfrowe Przetwarzanie Sygnałów i Obrazów|
+|        Kajetan Krasoń 252767, Kacper Małkowski 252724            |
+--------------------------------------------------------------------
+Wybierz operację, którą chcesz wykonać:
+1. .
+2. .
+3. .
+""")
